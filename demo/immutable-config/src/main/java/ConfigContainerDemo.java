303fab40bb53ed76fe68a14da2023aea69035c52
Roland Huß <roland@ro14nd.de>
2017-11-08 16:49:25 +0100

    update

---
+++ demo/immutable-config/src/main/java/ConfigContainerDemo.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+import javax.servlet.http.*;
+
+public class ConfigContainerDemo extends HttpServlet {
+
+    protected void doGet(HttpServletRequest req, 
+                         HttpServletResponse resp)
+        throws IOException {
+
+        Properties config = getConfig();
+
+        resp.getWriter().write(
+            "<html><body style='background-color: " +
+            config.get("color") +
+            "; font-size: 5em'><h1>" +
+            config.get("label") +
+            "</h1></body></html>");
+    }
+
+    private Properties getConfig() throws IOException {
+        Properties props = new Properties();
+        props.load(new FileInputStream("/config/demo.properties"));
+        return props;
+    }
+}
